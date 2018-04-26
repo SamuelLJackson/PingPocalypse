2e695e143ff6ddd9e3f599a199e4196e11871200
samuel everett <smev@samuels-MBP.rdacorp.com>
2018-04-26 10:06:23 -0400

    cleaning up dependencies

--- Awesome/src/components/LoggedInPlayers/Menu.js
+++ Awesome/src/components/LoggedInPlayers/Menu.js
@@ -6,12 +6,11 @@
 import React, { Component } from 'react';
 import {
   Text, View, StyleSheet, Image, ScrollView,
-  TouchableOpacity, Linking, Alert } from 'react-native';
+  TouchableOpacity, Alert } from 'react-native';
 import { getTheme } from 'react-native-material-kit';
 import EvilIcon from 'react-native-vector-icons/EvilIcons';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
-import SimpleIcon from 'react-native-vector-icons/SimpleLineIcons';
-import { MKTextField, MKColor, MKButton} from 'react-native-material-kit';
+import { MKButton} from 'react-native-material-kit';
 import {graphql, compose} from 'react-apollo';
 import gql from 'graphql-tag';
 import updateLoggedInPlayersView from '../graphql/updateLoggedInPlayersView';
@@ -36,11 +35,6 @@ const styles = StyleSheet.create({
       left: 80,
       fontSize: 22,
   },
-  title2: {
-      top: 35,
-      left: 82,
-      fontSize: 18,
-  },
   image: {
       flex: 0,
       height: 100,
@@ -61,45 +55,6 @@ const styles = StyleSheet.create({
       color: 'white',
       backgroundColor: 'rgba(255,255,255,0)',
   },
-  textArea: {
-      flexDirection: 'row',
-      paddingLeft: 20,
-      paddingTop: 10,
-      width: 260,
-  },
-  textIcons: {
-      color: '#26a69a',
-  },
-  actionArea: {
-      paddingTop: 10,
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-  },
-  editIcon: {
-      color: '#26a6e4',
-  },
-  sections: {
-      flexDirection:  'row',
-      paddingLeft: 10,
-      paddingTop: 10,
-      width: 100,
-  },
-  deleteIcon: {
-      color: '#e9a69a',
-  },
-  editDeleteArea: {
-    flexDirection:  'row',
-    paddingRight: 20,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: 'rgba(211,211,211, 0.3)',
-    marginBottom: 10,
-  },
-  buttonArea: {
-    height:100,
-    width:200
-  }
 });
 
 export const addMatchRequestMutation = gql`
